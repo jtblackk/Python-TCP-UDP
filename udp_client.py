9c7adc31907a64585ec48e9fca91d8136cf06496
Jeff Black <jtblack@g.clemson.edu>
2020-09-29 17:15:00 -0700

    added some more comments, finished udp_server

--- udp_client.py
+++ udp_client.py
@@ -4,7 +4,6 @@ import socket
 import time
 
 
-
 # PROGRAM OPTIONS
 # if set to true, this simulates the user pressing the bind button for the client (an unnecessary, but valid thing to do for the client)
 # if set to false, the client automatically binds the socket to a port upon sending
@@ -13,7 +12,7 @@ USER_PRESSES_BIND_BUTTON = False
 
 # if set to true, simulates a scenario where the user decided to let the client automatically send data in its send loop.
 # if set to False, then the user must press tell the client every time they wish to send data to the server
-USER_PRESSES_AUTO_SEND_BUTTON = True
+USER_PRESSES_AUTO_SEND_BUTTON = False
 
 #(only applies if USER_PRESSES_AUTO_SEND_BUTTON = False)
 # if set to true, simulates a scenario where the user chooses to type in a message to send to the server
@@ -35,24 +34,26 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # (optional) step 2: bind the socket to the client's ip address and one of it's ports
 # client ip address + random port are automatically assigned to the socket later if not bound.
 # IN-GAME:  the user can opt in to press a "bind" button and go through bind process. or they can just move on to the next step.
-#           after they press "bind," the port on the module's port that they specified should visually open up. if they don't press bind,
-#           then no ports open yet. 
+#           after they press "bind" and type in the port to bind to, the port on the module's port that they specified should 
+#           visibly open up. if they don't press bind, then no ports open yet. 
 if USER_PRESSES_BIND_BUTTON:
     # ask user to enter a port
-    client_address = input("please enter the ip address of this module: ")
-    client_port = int(input("please enter a port to use on this module: "))
+    client_address = input("enter the ip address of this module: ")
+    client_port = int(input("enter a port to use on this module: "))
 
     # bind the socket to the desired port
     sock.bind((client_address, client_port))
 
 # DEBUGGING: prints out the address & port bound to the socket
-print("\tsocket address & port before sending data:", sock.getsockname())
+print("\tclient socket address & port before sending data:", sock.getsockname())
 
 
 
 
-# --------- user should now open up a bound socket on the server module, unless they have already bound a socket on the server --------- #
-# if the user does not open up a bound socket on the server module, they can send data to a port on the server, but the server won't accept the data. the data will just never make it
+
+# --------- user should now open up a bound socket on the server module, unless they've already done so --------- #
+# if the user does not open up a bound socket on the server module, they can send data to a 
+# port on the server, but the server won't accept the data. the data will just never make it
 # to its final destination (e.g., in game the sent datagram might just fall to the ground after colliding with a closed port)
 
 
@@ -62,12 +63,17 @@ print("\tsocket address & port before sending data:", sock.getsockname())
 # -------------- sending data -------------------- #
 
 # step 3: get the server socket's address & port
+# IN-GAME: user will be prompted to type in which port they want to use on the server module
 server_address = input("please enter the server socket's ip address: ")
 server_port = int(input("please enter the server socket's port number: "))
 
 
 # step 4: send data to the server.
-# IN-GAME: if the user hasn't bound the client socket to a port, the module will automatically choose a port on the cli1ent's first attempt to "send." that port will visibly open up on the module. 
+# IN-GAME: if the user hasn't bound the client socket to a port, the module will automatically choose a port on the cli1ent's 
+# first attempt to "send." that port will visibly open up on the module. when the user presses "send," a visible packet will
+# fly from the client's opened port to the server's opened port. the user can step in-between the client and the server and
+# obstruct the dataflow. 
+
 # option 1: send data automatically. just sending a message with an incrementing number, with a 2-second pause between messages
 if USER_PRESSES_AUTO_SEND_BUTTON: 
     count = 1
@@ -81,18 +87,16 @@ if USER_PRESSES_AUTO_SEND_BUTTON:
         print("\tdata sent:", "\'" + data_to_send + "\'")
         print("\tdata sent from:", sock.getsockname())
         print("\tdata sent to:", (server_address, server_port))
-        print("---------------------------------------------------------------")
+        print("---------------------------------------------------------------\n")
 
         # update message count and wait before next send
         count += 1
         time.sleep(2)
-        
 # option 2: send data manually (user must press "send" every time they want to send data to the server)
 else:
     count = 1;
     while(True):
-        # prepare the data being sent
-        # if set to true, simulates a scenario where the user chooses to send a 
+        # prepare the data being sent (based on whether or not custom messages are being sent)
         if USER_SENDS_CUSTOM_MESSAGE:
                 data_to_send = input("enter a message to send: ")
         else:
@@ -108,5 +112,5 @@ else:
         print("\tdata sent:", "\'" + data_to_send + "\'")
         print("\tdata sent from:", sock.getsockname())
         print("\tdata sent to:", (server_address, server_port))
-        print("---------------------------------------------------------------")
+        print("---------------------------------------------------------------\n")
         
--- udp_server.py
+++ udp_server.py
@@ -1,10 +1,18 @@
+# program that models the behavior of the server module when using UDP
+
+
 import socket
 
-#PROGRAM OPTIONS
 
+#PROGRAM OPTIONS
+# if true, the server automatically processes datagrams sent to it.
+# if false, the server only processes a datagram after the user presses enter
 USER_PRESSES_AUTO_RECEIVE_BUTTON = True
 
 BUFFER_SIZE = 1024
+
+
+
 
 
 # ----------- server socket setup ----------- #
@@ -15,6 +23,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 # step 2: bind the socket to a port on the server module
+#IN-GAME: after they press "bind" and type in the port to bind to, 
+# the port on the module's port that they specified should visibly open up.
+
 # ask for socket info
 server_address = input("please enter the ip address of this module: ")
 server_port = int(input("please enter a port to use on this module: "))
@@ -23,23 +34,29 @@ server_port = int(input("please enter a port to use on this module: "))
 sock.bind((server_address, server_port))
 
 # DEBUGGING: prints out the address & port bound to the socket
-print("\tsocket address & port before sending data:", sock.getsockname())
+print("\tserver socket address & port before receiving data:", sock.getsockname())
+
+
+
 
 
 # ----------- receive data ------------ #
 
-if USER_PRESSES_AUTO_RECEIVE_BUTTON:
-    while(True):
-        # receive data
-        data_received, data_sender_socket = sock.recvfrom(BUFFER_SIZE)
-        
-        # do something with the data
+# step 4: receive and process data
+#IN-GAME: a datagram flies from the client into the server via the opened port. when the
+#         user presses the "receive" button, the packet gets processed and server does
+#         something with the message (e.g., prints it out into the game world) 
+while(True):
+    if not USER_PRESSES_AUTO_RECEIVE_BUTTON:
+        input("press enter to receive data")
 
-        # DEBUGGING: just some useful information about what's happening on the client side
-        print("---------------------------------------------------------------")
-        print("\tdata received:", "\'" + data_received.decode('utf-8') + "\'")
-        print("\tdata received from:", data_sender_socket)
-        # print("\tdata sent to:", (server_address, server_port))
-        print("---------------------------------------------------------------")
-else:
-    print("need to handle this case")
+    # receive data
+    data_received, data_sender_socket = sock.recvfrom(BUFFER_SIZE)
+    
+    # do something with the data
+
+    # DEBUGGING: just some useful information about what's happening on the server side
+    print("---------------------------------------------------------------")
+    print("\tdata received:", "\'" + data_received.decode('utf-8') + "\'")
+    print("\tdata received from:", data_sender_socket)
+    print("---------------------------------------------------------------")
